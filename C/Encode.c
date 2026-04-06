6104e3e3617087deae254aafae2ca63b479d739b
Gabriella Noble <gabriellan101@vt.edu>
2026-04-05 20:30:14 -0400

    Base case for encode

--- C/Encode.c
+++ C/Encode.c
@@ -1,4 +1,28 @@
 #include "Encode.h"
+#include <stdint.h>
 /*
 Primary encode/encapsulation function
+
+long long used for overflow safety
 */
+void Encode(long long arrayLen, const uint16_t *R, const uint16_t *Moduli, unsigned char *out){
+
+    //base case 
+    if (arrayLen == 1) // only encoding one num
+    {
+        uint16_t r = R[0];
+        uint16_t m = Moduli[0];
+
+        while (m > 1) // exit condition 
+        {
+            *out++ = r; // output is array value encoded 
+            r >>=8;
+            m = (m+255)>>8;
+        }
+    }
+
+    if (arrayLen > 1) //combining nums from array to then encode 
+    {
+
+    }
+}
--- C/Encode.h
+++ C/Encode.h
@@ -1,2 +1,5 @@
 #ifndef ENCODE_H
 #define ENCODE_H
+#include <stdint.h>
+
+void Encode(long long len, const uint16_t *R, const uint16_t *Moduli, unsigned char *out);
