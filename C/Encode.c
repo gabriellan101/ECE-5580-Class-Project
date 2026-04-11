d38b1416a22a908fc94d25a4636a353db3c75ab3
Gabriella Noble <gabriellan101@vt.edu>
2026-04-11 17:59:45 -0400

    Decode example

--- C/Encode.c
+++ C/Encode.c
@@ -26,22 +26,22 @@ void Encode(long long arrayLen, const uint16_t *R, const uint16_t *Moduli, unsig
         uint16_t R2[(arrayLen+1)/2]; 
         uint16_t M2[(arrayLen+1)/2];
         long long i;
-    for (i = 0;i < arrayLen-1;i += 2) {
-      uint32_t m0 = Moduli[i];
-      uint32_t r = R[i]+R[i+1]*m0;
-      uint32_t m = Moduli[i+1]*m0;
-      while (m >= 16384) {
-        *out++ = r;
-        r >>= 8;
-        m = (m+255)>>8;
-      }
-      R2[i/2] = r;
-      M2[i/2] = m;
-    }
-    if (i < arrayLen) {
-      R2[i/2] = R[i];
-      M2[i/2] = Moduli[i];
-    }
-    Encode(out,R2,M2,(arrayLen+1)/2);
+        for (i = 0;i < arrayLen-1;i += 2) {
+            uint32_t m0 = Moduli[i];
+            uint32_t r = R[i]+R[i+1]*m0;
+            uint32_t m = Moduli[i+1]*m0;
+            while (m >= 16384) {
+                *out++ = r;
+                r >>= 8;
+                m = (m+255)>>8;
+            }
+            R2[i/2] = r;
+            M2[i/2] = m;
+        }   
+        if (i < arrayLen) {
+            R2[i/2] = R[i];
+            M2[i/2] = Moduli[i];
+        }
+        Encode(out,R2,M2,(arrayLen+1)/2);
     }
 }
