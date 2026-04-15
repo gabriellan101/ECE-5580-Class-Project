f48eb19ae7024c96eaa32c2c13d4a1336322dccf
Gabriella Noble <gabriellan101@vt.edu>
2026-04-15 13:48:12 -0400

    Updated encode

--- C/sources/Encode.c
+++ C/sources/Encode.c
@@ -8,10 +8,11 @@ long long used for overflow safety
 Compute → c=rh+m (mod q)
 */
 
-void Encode(unsigned char *out, const uint16_t *R, const uint16_t *M, long long len)
+void Encode( const uint16_t *R, const uint16_t *M, long long len, unsigned char *encoded)
 {
     // Base case (length of message to encode is 1)
-    if (len == 1) {
+    if (len == 1) 
+    {
 
         // accessing arrays 
         uint16_t r = R[0]; // single number being encoded into bytes 
@@ -19,13 +20,15 @@ void Encode(unsigned char *out, const uint16_t *R, const uint16_t *M, long long 
 
         // mod is not bound at 1
         
-        while (m > 1) {
+        while (m > 1) 
+        {
             // storing a 32-bit value into 8-bit slot 
             // only writing the lowest 8 bits to the output buffer 
-            *out = (unsigned char) r; // need to advance location of output 
-            out++; // moving pointer 
+            *encoded = (unsigned char) r;
             // shift for byte emitted 
             r >>= 8;
+             // need to advance location of output 
+            encoded++; // moving pointer 
             // calculating upper bound of mod divided by 256 
             m = (m + 255) >> 8;
         }
@@ -33,18 +36,20 @@ void Encode(unsigned char *out, const uint16_t *R, const uint16_t *M, long long 
     
 
     // continuously half the problem by combining pairs 
-    while (len > 1) {
-
+    while (len > 1) 
+    {
         // halfing length of array (due to combination of pairs)
-        long long newlen = (len + 1) / 2;
+        long long halfLen = (len + 1) / 2;
+        long long i; // index variable
         
-        uint16_t R2[newlen]; // new integer array 
-        uint16_t M2[newlen]; // new moduli array
+        uint16_t newR[halfLen]; // new integer array 
+        uint16_t newM[halfLen]; // new moduli array
 
-        long long i; // index variable 
+         
 
         // loop through 
-        for (i = 0; i < len - 1; i += 2) {
+        for (i = 0; i < len - 1; i += 2) 
+        {
             uint32_t m0 = M[i]; // bound
             // mixed radix packing to combine pairs 
             // R[i+1]∈[0,M[i+1])
@@ -53,34 +58,41 @@ void Encode(unsigned char *out, const uint16_t *R, const uint16_t *M, long long 
 
             // when the mod range is large --> r safe to emit byte
             // divide r & m by 256, round up for m, and emit lowest byte for r 
-            while (m >= 16384) {
-                *out++ = (unsigned char) r;
+            while (m >= 16384) 
+            {
+                *encoded = (unsigned char) r; // byte to emit
                 // shift 
                 r >>= 8; 
+                // need to advance location of output 
+                encoded++; // moving pointer
                 // calculating upper bound of mod divided by 256 
                 m = (m + 255) >> 8;
             }
-
             // store reduced values calculated 
             // store new integers
-            R2[i / 2] = (uint16_t) r;
+            newR[i / 2] = (uint16_t) r;
             // store new moduli 
-            M2[i / 2] = (uint16_t) m;
+            newM[i / 2] = (uint16_t) m;
         }
 
         // handling unpaired elements --> carrying forward 
-        if (i < len) {
-            R2[i / 2] = R[i];
-            M2[i / 2] = M[i];
+        if (i < len) 
+        {
+            newR[i / 2] = R[i];
+            newM[i / 2] = M[i];
         }
 
         // overwriting original input arrays 
-        for (i = 0; i < newlen; i++) {
-            ((uint16_t *)R)[i] = R2[i];
-            ((uint16_t *)M)[i] = M2[i];
+        uint16_t *rPoint = (uint16_t *)R;
+        uint16_t *mPoint = (uint16_t *)M;
+
+        for (i = 0; i < halfLen; i++) 
+        {
+            *rPoint++ = newR[i];
+            *mPoint++ = newM[i];
         }
         // starts the new smaller problem; base case will not hit this
-        len = newlen;
+        len = halfLen;
     }
 
     
