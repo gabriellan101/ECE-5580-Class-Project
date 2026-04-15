a2a4ef9310aac47ee0d085d044330fb4956a4875
Joshua Dunn <joshrdunn@vt.edu>
2026-04-14 23:41:13 -0400

    added hash functions from reference implementation

--- C/includes/OuterLayer.h
+++ C/includes/OuterLayer.h
@@ -9,9 +9,50 @@
 #include "polyMult.h"
 #include "polyMult.h"
 #include "polyUtils.h"
+#include <stdlib.h>
+#include "../test_package/openssl_files/openssl/sha.h"
+#include <stddef.h>
 
+/*
+This file defines the outer layer of NTRUPrime, which interfaces with real world applications. The secret key is sent
+out of band in practice. This file also includes definitions of hash functions used in the reference implementation to allow
+comparisons with the KATs. We chose to do this instead of finding an external library for hash functions for this reason.
+*/
 
+static void OuterKeyGen(unsigned char *pk, unsigned char *sk);
 
+// The following function calls the above function in the NTRUPrime process and the outputs are used for encap and decap
+static void KEM_keygen(unsigned char *pk, unsigned char *sk);
+
+static void OuterEncrypt(unsigned char *CT, const F3 r, const unsigned char *sk);
+
+static void OuterDecrypt(F3 *r, const unsigned char *CT, const unsigned char *sk);
+
+static void Encap(unsigned char *CT, unsigned char *k, const unsigned char *pk);
+
+static void Decap(unsigned char *k, const unsigned char *CT, const unsigned char *sk);
+
+/*
+Hash functions from reference implementation below this line
+*/
+
+#define Ciphertexts_bytes 1007
+#define Hash_bytes 32
+#define Confirm_bytes 32
+#define Inputs_bytes ((P+3)/4)
+
+extern int crypto_hash_sha512(unsigned char *,const unsigned char *,unsigned long long);
+
+// I had to edit this function to remove errors
+static void Hash_prefix(unsigned char *out,int b,const unsigned char *in,int inlen);
+
+/* h = HashConfirm(r,pk,cache); cache is Hash4(pk) */
+static void HashConfirm(unsigned char *h,const unsigned char *r,const unsigned char *pk,const unsigned char *cache);
+
+/* ----- session-key hash */
+
+/* k = HashSession(b,y,z) */
+static void HashSession(unsigned char *k,int b,const unsigned char *y,const unsigned char *z);
 
 #include "../sources/OuterLayer.c"
 
--- C/sources/OuterLayer.c
+++ C/sources/OuterLayer.c
@@ -1,1 +1,48 @@
 #include "../includes/OuterLayer.h"
+
+
+
+int crypto_hash_sha512(unsigned char *out,const unsigned char *in,unsigned long long inlen)
+{
+  SHA512(in,inlen,out);
+  return 0;
+}
+
+static void Hash_prefix(unsigned char *out,int b,const unsigned char *in,int inlen)
+{
+    unsigned char *x;
+    unsigned char h[64];
+    int i;
+
+    x = (unsigned char *)malloc(inlen + 1);
+    if (!x) return; //allocation failure handling
+
+    x[0] = b;
+    for (i = 0;i < inlen;++i) x[i+1] = in[i];
+    crypto_hash_sha512(h,x,inlen+1);
+    for (i = 0;i < 32;++i) out[i] = h[i];
+
+    free(x);
+}
+
+static void HashConfirm(unsigned char *h,const unsigned char *r,const unsigned char *pk,const unsigned char *cache)
+{
+  unsigned char x[Hash_bytes*2];
+  int i;
+
+  Hash_prefix(x,3,r,Inputs_bytes);
+  for (i = 0;i < Hash_bytes;++i) x[Hash_bytes+i] = cache[i];
+
+  Hash_prefix(h,2,x,sizeof x);
+}
+
+static void HashSession(unsigned char *k,int b,const unsigned char *y,const unsigned char *z)
+{
+  unsigned char x[Hash_bytes+Ciphertexts_bytes+Confirm_bytes];
+  int i;
+
+  Hash_prefix(x,3,y,Inputs_bytes);
+  for (i = 0;i < Ciphertexts_bytes+Confirm_bytes;++i) x[Hash_bytes+i] = z[i];
+
+  Hash_prefix(k,b,x,sizeof x);
+}
