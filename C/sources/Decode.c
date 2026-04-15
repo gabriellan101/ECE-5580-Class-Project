81f0a043bb85bf9c662b4fd8d12a6f6d011e7b17
Gabriella Noble <gabriellan101@vt.edu>
2026-04-15 16:03:58 -0400

    Decode fix

--- C/sources/Decode.c
+++ C/sources/Decode.c
@@ -3,145 +3,154 @@
 /*
 Primary decode/decapsulation function
 
+must use reverse iteration 
+
+Steps: 
+
+1. read combined value in bytes from encoded 
+2. split back into separate values 
+3. repeat 
 */
 
-void Decode(uint16_t *out,const unsigned char *S,const uint16_t *M,long long len)
+
+void Decode(const unsigned char *encoded, const uint16_t *M,long long len, uint16_t *decoded)
 {
-  if (len == 1) {
-    if (M[0] == 1)
-      *out = 0;
-    else if (M[0] <= 256)
-      *out = uint32_mod_uint14(S[0],M[0]);
-    else
-      *out = uint32_mod_uint14(S[0]+(((uint16_t)S[1])<<8),M[0]);
-  }
-  if (len > 1) {
-    uint16_t R2[(len+1)/2];
-    uint16_t M2[(len+1)/2];
-    uint16_t bottomr[len/2];
-    uint16_t bottomt[len/2];
+    // working buffer for decoded val
+    uint16_t R[len];
+    // need buffer that we can modify --> M const 
+    // buffer for moduli (non constant)
+    uint16_t Mbuf[len]; 
+    // index 
     long long i;
-    for (i = 0;i < len-1;i += 2) {
-      uint32_t m = M[i]*(uint32_t) M[i+1];
-      if (m > 256*16383) {
-        bottomt[i/2] = 256*256;
-        bottomr[i/2] = S[0]+256*S[1];
-        S += 2;
-        M2[i/2] = (((m+255)>>8)+255)>>8;
-      } else if (m >= 16384) {
-        bottomt[i/2] = 256;
-        bottomr[i/2] = S[0];
-        S += 1;
-        M2[i/2] = (m+255)>>8;
-      } else {
-        bottomt[i/2] = 1;
-        bottomr[i/2] = 0;
-        M2[i/2] = m;
-      }
-    }
-    if (i < len)
-      M2[i/2] = M[i];
-    Decode(R2,S,M2,(len+1)/2);
-    for (i = 0;i < len-1;i += 2) {
-      uint32_t r = bottomr[i/2];
-      uint32_t r1;
-      uint32_t r0;
-      r += bottomt[i/2]*R2[i/2];
-      uint32_divmod_uint14(&r1,&r0,r,M[i]);
-      r1 = uint32_mod_uint14(r1,M[i+1]); /* only needed for invalid inputs */
-      *out++ = r0;
-      *out++ = r1;
-    }
-    if (i < len)
-      *out++ = R2[i/2];
-  }
-}
+    //current length -- number of values 
+    long long numVal = len;
 
-
-
-void Decode(uint16_t *out, const unsigned char *in, const uint16_t *M, long long len)
-{
-    /* Temporary working buffers */
-    uint16_t R[2][len];
-    uint16_t Mbuf[2][len];
-
-    int cur = 0, next = 1;
-
-    long long i;
-
-    /* Initialize M buffer */
-    for (i = 0; i < len; i++) {
-        Mbuf[cur][i] = M[i];
-    }
-
-    long long curlen = len;
-
-    /* Step 1: Build combined values from bytes (reverse of Encode shrinking) */
-    while (curlen > 1) {
-        long long newlen = (curlen + 1) / 2;
-
-        for (i = 0; i < newlen; i++) {
-            R[cur][i] = 0;
-        }
-
-        /* Reconstruct combined values */
-        for (i = newlen - 1; i >= 0; i--) {
-            uint32_t m = Mbuf[cur][i];
-            uint32_t r = 0;
-            uint32_t shift = 1;
-
-            /* Rebuild r from bytes */
-            while (m > 1) {
-                m = (m + 255) >> 8;
-                r += (*in++) * shift;
-                shift <<= 8;
-            }
-
-            R[cur][i] = (uint16_t) r;
-        }
-
-        /* Step 2: Split combined values into pairs */
-        for (i = 0; i < curlen - 1; i += 2) {
-            uint32_t m0 = Mbuf[cur][i];
-            uint32_t r = R[cur][i / 2];
-
-            R[next][i]     = r % m0;
-            R[next][i + 1] = r / m0;
-
-            Mbuf[next][i]     = m0;
-            Mbuf[next][i + 1] = Mbuf[cur][i + 1];
-        }
-
-        /* Handle odd element */
-        if (i < curlen) {
-            R[next][i] = R[cur][i / 2];
-            Mbuf[next][i] = Mbuf[cur][i];
-        }
-
-        /* Swap buffers */
-        cur ^= 1;
-        next ^= 1;
-
-        curlen = newlen;
-    }
-
-    /* Final step: last element */
+    // need to copy initial mod for reorg later
+    
+    // populating mod buffer by copying moduli from M passed in
+    for (i = 0; i < len; i++) 
     {
-        uint32_t m = Mbuf[cur][0];
-        uint32_t r = 0;
-        uint32_t shift = 1;
-
-        while (m > 1) {
-            m = (m + 255) >> 8;
-            r += (*in++) * shift;
-            shift <<= 8;
-        }
-
-        R[cur][0] = (uint16_t) r;
+      Mbuf[i] = M[i];
     }
+    // main loop -- byte reconstruction
+    /*
+      Need to do: 
+      1. read comp val
+      2. expand 
+      3. reduce prob
+    */
+    // more than one val pair to decode 
+    while (numVal > 1) 
+    {
+      // calculate half len
+      // each pair --> 1 val 
+      // pass over odd el
+      long long halfLen = (numVal + 1) / 2;
+      // length for iteration 
+      long long halfLen1 = halfLen - 1;
 
-    /* Output final values */
-    for (i = 0; i < len; i++) {
-        out[i] = R[cur][i];
+      //reconstruction
+      // working right to left -- reverse iteration
+      for (i = halfLen1; i >= 0; i--) 
+      {
+        // current mod --> bound 
+        uint32_t currMod = Mbuf[i];
+        //inititialize for reconstruct int 
+        uint32_t r = 0;
+        // tracker for power 256
+        uint32_t powTrack = 1;
+
+        // condition to check --> if more bytes are needed 
+        while (currMod > 1) 
+        {
+          // shrinking mod --> determine bytes 
+          // need val for currMod + 255 
+          uint32_t currMod1 = currMod + 255;
+          // shrinking mod --> dividing by 256
+          // maybe >> 8 ?
+          currMod = (currMod1) / 256;
+          // rounding next byte from input 
+          // multiplying by current power of 256 
+          // add into r 
+          // advance encoded pointer
+          uint32_t outByte = *encoded;
+          r += outByte * powTrack;
+          encoded++;
+          // multply shift by 256 for next byte
+          powTrack = powTrack * 256;
+        }
+        
+        // need to cast & store in array -- come back 
+        // storing new val 
+        R[i] = (uint16_t)r;
+      }
+
+      // now expanding pairs 
+      // processing pairs as (i-1, i)
+      // need to avoid overwriting 
+      for (long long i = numVal - 1; i > 0; i -= 2) 
+      {
+        // mod for first num in pair 
+        uint32_t modNum1 = Mbuf[i - 1];
+        // combined val of pair 
+        // pair index 
+        long long indexPair = (i - 1) / 2;
+        uint32_t r  = R[indexPair];
+        
+        // need to split 
+        // div ? 
+        // grabbing first val 
+        uint32_t firstVal = r % modNum1;
+        // storing val 
+        R[i - 1] = firstVal; 
+        // grabbing second val  
+        uint32_t secondVal = r / modNum1; 
+        // storing second val 
+        R[i] = secondVal;
+        // storing both mods 
+        // mod for first val
+        Mbuf[i - 1] = modNum1;
+        // mod corresponding second val 
+        // shouldn't change ?
+        // Mbuf[i] = Mbuf[i];       
+      }
+      // checking for odd el
+      // not paired --> need to copy
+      // mod should be fine 
+      if (numVal % 2 != 0) 
+      {
+        // index of last el
+        long long i = numVal - 1;
+        // copy not paired value (that should be the odd el)
+        // store in array
+        R[i] = R[i / 2];
+      }
+      // move to next smaller prob with updated len
+      numVal = halfLen;
+    }
+      //final mod 
+      uint32_t finalMod = Mbuf[0];
+      // final val 
+      uint32_t finalVal = 0;
+      // shifter for 256 pow
+      uint32_t pow256 = 1;
+      while (finalMod > 1) 
+      {
+        // calc bte count
+        finalMod = (finalMod + 255) / 256;
+        // read byte & grow
+        uint32_t byte = *encoded;
+        finalVal += byte * pow256;
+        encoded++;
+        // increase pow
+        pow256 = pow256 * 256;
+      }
+      // store final val
+      R[0] = (uint16_t)finalVal;
+    // copy val to output array 
+    for (long long i = 0; i < len; i++) 
+    {
+      // final encoded val
+      decoded[i] = R[i];
     }
 }
