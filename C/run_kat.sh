bd650004e39de9b45d5341f727f868ef85173444
Archit Chavan <architchavan@vt.edu>
2026-04-29 20:06:20 -0400

    Updated powershell script

--- C/run_kat.sh
+++ C/run_kat.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#powershell -ExecutionPolicy Bypass -File run_kat.ps1 <- runs the script if powershell has permission issues also powershell sucks
 cd sources
 clear
 gcc .\KAT_rng.c .\NTRUPrime.c .\polyArith.c .\polyMult.c .\polyUtils.c .\run_KAT.c .\Types.c .\rng.c -o test.exe -Iopenssl_files -Lopenssl_files -llibcrypto-3-x64 -I"../includes"
