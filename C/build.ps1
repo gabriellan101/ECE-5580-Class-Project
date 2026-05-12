cc8464099218c731037365fe40a4a0d1472d6bdd
Archit Chavan <architchavan@vt.edu>
2026-05-11 21:25:33 -0400

    Renamed build script

--- C/build.ps1
+++ C/build.ps1
@@ -1,4 +1,4 @@
-#powershell -ExecutionPolicy Bypass -File run_kat.ps1
+#powershell -ExecutionPolicy Bypass -File build.ps1
 clear
 gcc test_build.c sources/*.c -o test.exe -Iopenssl_files -Lopenssl_files -llibcrypto-3-x64
 echo compilation done
--- C/test_comp.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//#include "../includes/polyMult.h"
-//#include "../includes/polyUtils.h"
-//#include "../includes/rng.h"
-#include "../includes/NTRUPrime.h"
-int main() {
-    printf("done1\n");
-    return 0;
-}
