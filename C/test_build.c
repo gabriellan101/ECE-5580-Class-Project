56b2ece855623bd3ff090380c4aae321522d7f8e
Archit Chavan <architchavan@vt.edu>
2026-05-11 21:38:39 -0400

    working on full thing

--- C/test_build.c
+++ C/test_build.c
@@ -1,5 +1,11 @@
 #include "includes/OuterLayer.h"
-
+#include "includes/NTRUPrime.h"
 int main() {
+    
+    Fq h[761], c[761];
+    F3 f[761], ginv[761];
+    KeyGen(h,f,ginv);
+    //Encrypt(c, const F3 *r, h)
+
     printf("test ran\n");
 }
