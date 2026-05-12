2d362ba07feb89a0291e9bcc5f4d324c9a4f4330
Archit Chavan <architchavan@vt.edu>
2026-05-12 13:17:48 -0400

    Finished Kat testing framework

--- C/kat_ref.c
+++ C/kat_ref.c
@@ -5,6 +5,8 @@
    + mods from djb: see KATNOTES
 */
 
+char seed1[48] = {0x06,0x15,0x50,0x23,0x4D,0x15,0x8C,0x5E,0xC9,0x55,0x95,0xFE,0x04,0xEF,0x7A,0x25,0x76,0x7F,0x2E,0x24,0xCC,0x2B,0xC4,0x79,0xD0,0x9D,0x86,0xDC,0x9A,0xBC,0xFD,0xE7,0x05,0x6A,0x8C,0x26,0x6F,0x9E,0xF9,0x7E,0xD0,0x85,0x41,0xDB,0xD2,0xE1,0xFF,0xA1};
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,6 +32,12 @@ main()
     unsigned char *pk = 0;
     unsigned char *sk = 0;
 
+    FILE * out_file = fopen("kat_out.txt", "w");
+    if(out_file == NULL) {
+        printf("ERROR: File not created/found\n");
+        return -1;
+    }
+
     for (i=0; i<48; i++)
         entropy_input[i] = i;
     randombytes_init(entropy_input, NULL, 256);
@@ -50,13 +58,58 @@ main()
         if (!pk) abort();
         if (!sk) sk = malloc(crypto_kem_SECRETKEYBYTES);
         if (!sk) abort();
-        //printf("cp1\n");
+        
+
+        /**
+         * Generate Seed and Store in File
+         */
         randombytes_init(seed[i], NULL, 256);
         
-        OuterKeyGen(pk,sk);
+        fprintf(out_file, "count = %i\n", i);
+        fprintf(out_file, "seed = ");
+        for(int c = 0; c< 48; c++) {
+            fprintf(out_file, "%02hhx", seed[i][c]);
+        }
+        fprintf(out_file, "\n");
         
+        /**
+         * Calculate Pk and Sk
+         */
+        OuterKeyGen(pk,sk);
+
+        /**
+         * Store Pk in file
+         */
+        fprintf(out_file, "pk = ");
+        for(int c = 0; c< crypto_kem_PUBLICKEYBYTES; c++) {
+            fprintf(out_file, "%02hhx", pk[c]);
+        }
+        fprintf(out_file, "\n");
+
+        /**
+         * Store Sk in file
+         */
+        fprintf(out_file, "sk = ");
+        for(int c = 0; c< crypto_kem_SECRETKEYBYTES; c++) {
+            fprintf(out_file, "%02hhx", sk[c]);
+        }
+        fprintf(out_file, "\n");
+  
+
         OuterEncrypt(ct, ss, pk);
         
+        fprintf(out_file, "ct = ");
+        for(int c = 0; c< crypto_kem_CIPHERTEXTBYTES; c++) {
+            fprintf(out_file, "%02hhx", ct[c]);
+        }
+        fprintf(out_file, "\n");
+
+        fprintf(out_file, "ss = ");
+        for(int c = 0; c< crypto_kem_BYTES; c++) {
+            fprintf(out_file, "%02hhx", ss[c]);
+        }
+        fprintf(out_file, "\n");
+
         OuterDecrypt(ss1, ct, sk);
         
         if ( memcmp(ss, ss1, crypto_kem_BYTES) ) {
